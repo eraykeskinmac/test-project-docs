3ab6bdcaa1edfcca5d7e6ad273a237d5cff80c3d
eraykeskinmac <eraykeskinmac@gmail.com>
2025-11-19 13:19:18 +0300

    feat: Switch to page-level auth without middleware

--- .nexusdocs/app/page.tsx
+++ .nexusdocs/app/page.tsx
@@ -1,5 +1,12 @@
 import { redirect } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const session = await getServerSession();
+  
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+  
   redirect('/docs');
 }
